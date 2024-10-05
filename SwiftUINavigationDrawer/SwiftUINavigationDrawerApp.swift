2fddb800b816a149d47ab7476d3ae6464f727dc4
manuaravind <manuaravindpta@gmail.com>
2024-10-05 22:13:24 +0530

    Initial Commit

---
+++ SwiftUINavigationDrawer/SwiftUINavigationDrawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationDrawerApp.swift
+//  SwiftUINavigationDrawer
+//
+//  Created by Manu Aravind on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationDrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
